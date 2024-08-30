618f2be6b5dd728be1ec33a4b8a79ac331b947f7
Diana Avedisyan <diana-avedisyan@mail.ru>
2024-08-30 14:24:15 +0500

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
+'use strict';
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyInterest = percent / 100 / 12;
+  let remainingAmount = amount - contribution;
+  let monthlyPayment = remainingAmount * (monthlyInterest + (monthlyInterest / (((1 + monthlyInterest) ** countMonths) - 1)));
+  return parseFloat((monthlyPayment * countMonths).toFixed(2));
 }

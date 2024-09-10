90052b3f3ca62dc7e6dd3c88d58fcdcc1b90220a
Diana Avedisyan <diana-avedisyan@mail.ru>
2024-09-10 13:40:16 +0500

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if ('marks' in this) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!('marks' in this) || !this.marks.length) {
+    return 0;
+  }
+  return this.marks.reduce((currentSum, currentValue) => currentSum + currentValue, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }

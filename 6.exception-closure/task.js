fde0855399919b571269cf7d90aaaf2acf3c8c4a
Diana Avedisyan <diana-avedisyan@mail.ru>
2024-09-17 17:23:22 +0500

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(number) {
+  if (Number.isNaN(parseFloat(number))) {
+    throw new Error('Невалидное значение');
+  }
+  return parseFloat(number);
+}
+
+function validateCount(number) {
+  try {
+    return parseCount(number);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(sideA, sideB, sideC) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+
+    if (this.sideA + this.sideB < this.sideC || this.sideB + this.sideC < this.sideA || this.sideA + this.sideC < this.sideB) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  get perimeter() {
+    return this.sideA + this.sideB + this.sideC;
+  }
+
+  get area() {
+    return parseFloat(Math.sqrt(this.perimeter / 2 * (this.perimeter / 2 - this.sideA) * (this.perimeter / 2 - this.sideB) * (this.perimeter / 2 - this.sideC)).toFixed(3));
+  }
+}
+
+function getTriangle(sideA, sideB, sideC) {
+  try {
+    return new Triangle(sideA, sideB, sideC);
+  } catch (error) {
+    return { 
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      }
+    };
+  }
+}
